f428a8a35cdb2641ff6a673ed7708f6f3a5f910b
김명이 <myeongyevillain@gmail.com>
2026-03-19 16:02:38 +0900

    과제1 코딩과 테스트 완료 Label을 맨 위쪽에 잘 보이게 배치하였고, Listbox를 생성해 화면 중앙 살짝 왼쪽에 위치하게 하였습니다. Listbox를 의도적으로 살짝 왼쪽으로 배치하여 나중에 새로운 기능 추가가 용이하게 하였습니다. 맨 아래쪽에 TextBox를 배치하였고, 그 오른쪽에 ENTER 텍스트를 넣은 버튼을 위치하였습니다. TextBox에 메시지를 입력하고 ENTER 버튼을 클릭하면 ListBox에 메시지가 추가됩니다. 아무 메시지도 입력하지 않았거나 공백(스페이스바)이라면'텍스트를 입력해주세요' 문구가 대신 입력되며 리스트에 추가되지 않습니다.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("궁서", 48F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.Location = new Point(12, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(617, 64);
+            label1.TabIndex = 0;
+            label1.Text = "ECHO MESSENGER";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Font = new Font("한컴 고딕", 24F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            textBox1.Location = new Point(12, 501);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(761, 49);
+            textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("한컴 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(12, 97);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(761, 389);
+            listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.White;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("한컴 고딕", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.Location = new Point(784, 501);
+            button1.Name = "button1";
+            button1.Size = new Size(116, 49);
+            button1.TabIndex = 3;
+            button1.Text = "ENTER";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            BackColor = Color.FromArgb(255, 255, 192);
+            ClientSize = new Size(912, 562);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Echo Messenger";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private ListBox listBox1;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,26 @@ namespace EchoMessenger
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string typedMessage;
+
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                textBox1.Text = "메시지를 입력해주세요.";
+
+            }
+            else
+            {
+                typedMessage = textBox1.Text;
+                listBox1.Items.Add(typedMessage);
+            }
+        }
     }
 }
